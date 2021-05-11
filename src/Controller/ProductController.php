96da8b9754916d78d2baa8ca6b539c1484924e0e
Olivier Floch <o.floch@protonmail.com>
2021-05-11 20:27:55 +0200

    feat: get Products list and Product detail

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,22 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
+    /**
+     * @throws ExceptionInterface
+     */
     #[Route(
         '/products',
         name: 'products_list',
         methods: ['GET']
     )]
-    public function getProducts(): Response
+    public function getProducts(ProductRepository $repository, SerializerInterface $serializer): Response
     {
-        return $this->json([
-            'message' => 'Get products list',
+        $products = $repository->findAll();
+
+        $products = $serializer->normalize($products, null, [
+            AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            },
         ]);
+
+        return $this->json($products);
     }
 
     #[Route(
@@ -26,10 +40,18 @@ class ProductController extends AbstractController
         requirements: ['id' => '\d+'],
         methods: ['GET']
     )]
-    public function getOneProduct(): Response
+    public function getOneProduct(int $id, ProductRepository $repository, SerializerInterface $serializer): Response
     {
-        return $this->json([
-            'message' => 'Get Product detail',
+        $product = $repository->findOneBy(['id' => $id]);
+        $product = $serializer->normalize($product, null, [
+            AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            },
+        ]);
+
+        return $this->json($product, 200, [], [
+            'groups' => 'show_product',
         ]);
     }
 }
--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -2,16 +2,42 @@
 
 namespace Test\Controller;
 
+use JsonException;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
 {
-    public function testGetProductsList(): void
+    public function testGetProductsListNotAuthenticated(): void
     {
         $client = static::createClient();
         $client->request('GET', '/api/products');
 
-        self::assertResponseIsSuccessful();
+        self::assertResponseStatusCodeSame(401, 'Products list is not accessible when user is not authenticated');
+        self::isJson();
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetProductsListAuthenticated(): void
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/login_check',
+            [],
+            [],
+            [json_encode(['name' => 'CompanyTest', 'password' => 'Test1234321'], JSON_THROW_ON_ERROR)]
+        );
+
+        $response = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        $client->request('GET', '/api/products', [
+            'Authorization' => 'Bearer '.$response['token'],
+        ]);
+
+        self::assertResponseStatusCodeSame(401, 'Products list is not accessible when user is not authenticated');
         self::isJson();
     }
 

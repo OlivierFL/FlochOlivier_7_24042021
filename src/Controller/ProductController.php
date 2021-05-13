5c3bad978e6c51c593e699c2e0edaf519b4a78a2
Olivier Floch <o.floch@protonmail.com>
2021-05-13 18:56:39 +0200

    refactor: handle normalization in ProductsService

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,11 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
+    public function __construct(private ProductsService $productsService)
+    {
+    }
+
     /**
      * @throws ExceptionInterface
      */
@@ -22,42 +24,36 @@ class ProductController extends AbstractController
         name: 'products_list',
         methods: ['GET']
     )]
-    public function getProducts(Request $request, ProductRepository $repository, ProductsService $productsService, SerializerInterface $serializer): Response
+    public function getProducts(Request $request, ProductRepository $repository): Response
     {
         $products = $repository->findAll();
         $page = max(1, $request->query->getInt('page', 1));
         $limit = $request->query->getInt('limit', 10);
 
-        $productsPaginated = $productsService->getProductsPaginated($products, $page, $limit);
-
-        $productsPaginated = $serializer->normalize($productsPaginated, null, [
-            AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
-            'circular_reference_handler' => function ($object) {
-                return $object->getId();
-            },
-        ]);
+        $productsPaginated = $this->productsService->getProductsPaginated($products, $page, $limit);
 
         return $this->json($productsPaginated);
     }
 
+    /**
+     * @throws ExceptionInterface
+     */
     #[Route(
         '/products/{id}',
         name: 'product_detail',
         requirements: ['id' => '\d+'],
         methods: ['GET']
     )]
-    public function getOneProduct(int $id, ProductRepository $repository, SerializerInterface $serializer): Response
+    public function getOneProduct(int $id, ProductRepository $repository): Response
     {
         $product = $repository->findOneBy(['id' => $id]);
-        $product = $serializer->normalize($product, null, [
-            AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
-            'circular_reference_handler' => function ($object) {
-                return $object->getId();
-            },
-        ]);
 
-        return $this->json($product, 200, [], [
-            'groups' => 'show_product',
-        ]);
+        if (!$product) {
+            return $this->json(['Error' => 'Product not found'], 404);
+        }
+
+        $product = $this->productsService->normalize($product);
+
+        return $this->json($product);
     }
 }
--- src/Service/ProductsService.php
+++ src/Service/ProductsService.php
@@ -3,19 +3,24 @@
 namespace App\Service;
 
 use App\Entity\Product;
-use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsService
 {
-    public function __construct(private PaginatorInterface $paginator)
-    {
+    public function __construct(
+        private PaginatorInterface $paginator,
+        private SerializerInterface $serializer
+    ) {
     }
 
     /**
      * @param Product[] $products
      * @param int       $page
      * @param int       $limit
+     *
+     * @throws ExceptionInterface
      *
      * @return array
      */
@@ -27,22 +32,41 @@ class ProductsService
             $limit
         );
 
-        return $this->formatResult($page, $productsPaginated, $products);
+        $productsPaginated = $this->normalize($productsPaginated);
+
+        $total = \count($products);
+
+        return $this->formatResult($productsPaginated, $total, $page, $limit);
     }
 
     /**
-     * @param int                 $page
-     * @param PaginationInterface $productsPaginated
-     * @param array               $products
+     * @param mixed $data
+     *
+     * @throws ExceptionInterface
+     *
+     * @return mixed
+     */
+    public function normalize(mixed $data): mixed
+    {
+        return $this->serializer->normalize($data, null, [
+            'circular_reference_handler' => fn ($object) => $object->getId(),
+        ]);
+    }
+
+    /**
+     * @param array $productsPaginated
+     * @param int   $total
+     * @param int   $page
+     * @param int   $limit
      *
      * @return array
      */
-    private function formatResult(int $page, PaginationInterface $productsPaginated, array $products): array
+    private function formatResult(array $productsPaginated, int $total, int $page, int $limit): array
     {
         $result = [];
         $result['page'] = $page;
-        $result['count'] = \count($productsPaginated);
-        $result['total'] = \count($products);
+        $result['limit'] = $limit;
+        $result['total'] = $total;
         $result['data'] = $productsPaginated;
 
         return $result;

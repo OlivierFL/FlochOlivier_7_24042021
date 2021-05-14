e6f2a2947a090003bfcded714f9ec1e8ee58383d
Olivier Floch <o.floch@protonmail.com>
2021-05-14 13:11:15 +0200

    test: add tests for ProductController

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -3,6 +3,7 @@
 namespace Test\Controller;
 
 use JsonException;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
@@ -21,32 +22,66 @@ class ProductControllerTest extends WebTestCase
      */
     public function testGetProductsListAuthenticated(): void
     {
-        $client = static::createClient();
+        $client = $this->createAuthenticatedClient();
 
+        $client->request('GET', '/api/products');
+
+        self::assertResponseIsSuccessful('Products list is accessible when user is authenticated');
+        self::isJson();
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetProductDetails(): void
+    {
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', '/api/products/106');
+
+        self::assertResponseIsSuccessful('Products detail is accessible when user is authenticated');
+        self::isJson();
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetProductDetailsProductNotExists(): void
+    {
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', '/api/products/1');
+
+        self::assertResponseStatusCodeSame(404, 'When Product does not exists, response code equals to 404');
+        self::isJson();
+    }
+
+    /**
+     * Create a client with a default Authorization header.
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @throws JsonException
+     * @return KernelBrowser
+     */
+    protected function createAuthenticatedClient(string $username = 'Company', string $password = 'Company1234'): KernelBrowser
+    {
+        $client = static::createClient();
         $client->request(
             'POST',
             '/api/login_check',
             [],
             [],
-            [json_encode(['name' => 'CompanyTest', 'password' => 'Test1234321'], JSON_THROW_ON_ERROR)]
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'username' => $username,
+                'password' => $password,
+            ], JSON_THROW_ON_ERROR)
         );
 
-        $response = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        $data = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
-        $client->request('GET', '/api/products', [
-            'Authorization' => 'Bearer '.$response['token'],
-        ]);
+        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
 
-        self::assertResponseStatusCodeSame(401, 'Products list is not accessible when user is not authenticated');
-        self::isJson();
-    }
-
-    public function testGetProductDetails(): void
-    {
-        $client = static::createClient();
-        $client->request('GET', '/api/products/1');
-
-        self::assertResponseIsSuccessful();
-        self::isJson();
+        return $client;
     }
 }
